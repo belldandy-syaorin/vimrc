c66fcb2067af9302c5a46c404e0ce6daa13a8c60
belldandy <belldandy@vm-debian.(none)>
2013-04-20 11:58:27 +0900

    2013-04-20_00 update

--- gvimwinpos.py
+++ gvimwinpos.py
@@ -12,5 +12,18 @@ gvimrect_w = gvimrect[2] - gvimrect_x
 gvimrect_h = gvimrect[3] - gvimrect_y
 center_x = (resolution_w - gvimrect_w) / 2
 center_y = (resolution_h - gvimrect_h) / 2
-vim.command("let g:gvimwinpos_x="+str(center_x))
-vim.command("let g:gvimwinpos_y="+str(center_y))
+center_left_x = 0
+center_right_x = (resolution_w - gvimrect_w)
+if gvimrect_x != center_x and gvimrect_y != center_y:
+        vim.command("let g:gvimwinpos_x="+str(center_x))
+        vim.command("let g:gvimwinpos_y="+str(center_y))
+        vim.command("let g:gvimwinpos_mode='center'")
+if gvimrect_x == center_x and gvimrect_y == center_y:
+        vim.command("let g:gvimwinpos_x="+str(center_left_x))
+        vim.command("let g:gvimwinpos_mode='center(left)'")
+if gvimrect_x == center_left_x and gvimrect_y == center_y:
+        vim.command("let g:gvimwinpos_x="+str(center_right_x))
+        vim.command("let g:gvimwinpos_mode='center(right)'")
+if gvimrect_x == center_right_x and gvimrect_y == center_y:
+        vim.command("let g:gvimwinpos_x="+str(center_x))
+        vim.command("let g:gvimwinpos_mode='center'")
--- gvimwinpos.vim
+++ gvimwinpos.vim
@@ -1,3 +1,3 @@
 if has('python')
-	nnoremap <F3> :execute 'pyfile ' . '$VIM/gvimwinpos.py'<CR>:execute 'winpos ' . g:gvimwinpos_x g:gvimwinpos_y<CR>:echo "gvimwinpos=center"<CR>
+	nnoremap <F3> :execute 'pyfile ' . '$VIM/gvimwinpos.py'<CR>:execute 'winpos ' . g:gvimwinpos_x g:gvimwinpos_y<CR>:echo "gvimwinpos_mode =" gvimwinpos_mode<CR>
 endif
