c9bc54cda60f1249faa5ee99696cdc14abda94a9
belldandy <belldandy@vm-debian.(none)>
2013-04-20 21:21:52 +0900

    2013-04-20_02 update

--- gvimwinpos.py
+++ gvimwinpos.py
@@ -1,5 +1,6 @@
 from win32api import GetSystemMetrics
 import win32gui
+import sys
 import vim
 
 resolution_w = GetSystemMetrics(0)
@@ -14,19 +15,61 @@ center_x = (resolution_w - gvimrect_w) / 2
 center_y = (resolution_h - gvimrect_h) / 2
 center_left_x = 0
 center_right_x = (resolution_w - gvimrect_w)
-if gvimrect_x != center_x and gvimrect_y != center_y:
+
+smart_x = [resolution_w / 3, resolution_w / 3 * 2, resolution_w]
+smart_y = [resolution_h / 3, resolution_h / 3 * 2, resolution_h]
+wincenter = [gvimrect_x + (gvimrect_w / 2), gvimrect_y + (gvimrect_h / 2)]
+resmart_x = [(resolution_w - gvimrect_w) / 2,resolution_w - gvimrect_w]
+resmart_y = [(resolution_h - gvimrect_h) / 2,resolution_h - gvimrect_h]
+
+if sys.argv[0] == "normal":
+    if gvimrect_x != center_x and gvimrect_y != center_y:
         vim.command("let g:gvimwinpos_x="+str(center_x))
         vim.command("let g:gvimwinpos_y="+str(center_y))
         vim.command("let g:gvimwinpos_mode='center'")
-if gvimrect_x == center_x and gvimrect_y == center_y:
+    if gvimrect_x == center_x and gvimrect_y == center_y:
         vim.command("let g:gvimwinpos_x="+str(center_left_x))
         vim.command("let g:gvimwinpos_y="+str(center_y))
         vim.command("let g:gvimwinpos_mode='center(left)'")
-if gvimrect_x == center_left_x and gvimrect_y == center_y:
+    if gvimrect_x == center_left_x and gvimrect_y == center_y:
         vim.command("let g:gvimwinpos_x="+str(center_right_x))
         vim.command("let g:gvimwinpos_y="+str(center_y))
         vim.command("let g:gvimwinpos_mode='center(right)'")
-if gvimrect_x == center_right_x and gvimrect_y == center_y:
+    if gvimrect_x == center_right_x and gvimrect_y == center_y:
         vim.command("let g:gvimwinpos_x="+str(center_x))
         vim.command("let g:gvimwinpos_y="+str(center_y))
         vim.command("let g:gvimwinpos_mode='center'")
+if sys.argv[0] == "smart":
+    if wincenter[0] > 0 and wincenter[0] < smart_x[0]:
+        vim.command("let g:gvimwinpos_x=0")
+        if wincenter[1] < smart_y[0]:
+            vim.command("let g:gvimwinpos_y=0")
+            vim.command("let g:gvimwinpos_mode='smart(7)'")
+        if wincenter[1] > smart_y[0] and wincenter[1] < smart_y[1]:
+            vim.command("let g:gvimwinpos_y="+str(resmart_y[0]))
+            vim.command("let g:gvimwinpos_mode='smart(4)'")
+        if wincenter[1] > smart_y[1] and wincenter[1] < smart_y[2]:
+            vim.command("let g:gvimwinpos_y="+str(resmart_y[1]))
+            vim.command("let g:gvimwinpos_mode='smart(1)'")
+    if wincenter[0] > smart_x[0] and wincenter[0] < smart_x[1]:
+        vim.command("let g:gvimwinpos_x="+str(resmart_x[0]))
+        if wincenter[1] < smart_y[0]:
+            vim.command("let g:gvimwinpos_y=0")
+            vim.command("let g:gvimwinpos_mode='smart(8)'")
+        if wincenter[1] > smart_y[0] and wincenter[1] < smart_y[1]:
+            vim.command("let g:gvimwinpos_y="+str(resmart_y[0]))
+            vim.command("let g:gvimwinpos_mode='smart(5)'")
+        if wincenter[1] > smart_y[1] and wincenter[1] < smart_y[2]:
+            vim.command("let g:gvimwinpos_y="+str(resmart_y[1]))
+            vim.command("let g:gvimwinpos_mode='smart(2)'")
+    if wincenter[0] > smart_x[1] and wincenter[0] < smart_x[2]:
+        vim.command("let g:gvimwinpos_x="+str(resmart_x[1]))
+        if wincenter[1] < smart_y[0]:
+            vim.command("let g:gvimwinpos_y=0")
+            vim.command("let g:gvimwinpos_mode='smart(9)'")
+        if wincenter[1] > smart_y[0] and wincenter[1] < smart_y[1]:
+            vim.command("let g:gvimwinpos_y="+str(resmart_y[0]))
+            vim.command("let g:gvimwinpos_mode='smart(6)'")
+        if wincenter[1] > smart_y[1] and wincenter[1] < smart_y[2]:
+            vim.command("let g:gvimwinpos_y="+str(resmart_y[1]))
+            vim.command("let g:gvimwinpos_mode='smart(3)'")
--- gvimwinpos.vim
+++ gvimwinpos.vim
@@ -1,3 +1,4 @@
 if has('python')
-	nnoremap <F3> :execute 'pyfile ' . '$VIM/gvimwinpos.py'<CR>:execute 'winpos ' . g:gvimwinpos_x g:gvimwinpos_y<CR>:echo "gvimwinpos_mode =" gvimwinpos_mode<CR>
+	nnoremap <F2> :python import sys<CR>:python sys.argv = ["smart"]<CR>:execute 'pyfile ' . '$VIM/gvimwinpos.py'<CR>:execute 'winpos ' . g:gvimwinpos_x g:gvimwinpos_y<CR>:echo "gvimwinpos_mode =" gvimwinpos_mode<CR>
+	nnoremap <F3> :python import sys<CR>:python sys.argv = ["normal"]<CR>:execute 'pyfile ' . '$VIM/gvimwinpos.py'<CR>:execute 'winpos ' . g:gvimwinpos_x g:gvimwinpos_y<CR>:echo "gvimwinpos_mode =" gvimwinpos_mode<CR>
 endif
