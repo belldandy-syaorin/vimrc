564b1f7a56c905706a15f93ac9481805e69429ad
belldandy <belldandy@vm-debian.(none)>
2013-04-22 21:17:15 +0900

    2013-04-22_00 update

--- gvimwinpos.py
+++ gvimwinpos.py
@@ -20,43 +20,65 @@ smart_y = [resolution_h / 3, resolution_h / 3 * 2, resolution_h]
 wincenter = [gvimrect_x + (gvimrect_w / 2), gvimrect_y + (gvimrect_h / 2)]
 resmart_x = [(resolution_w - gvimrect_w) / 2,resolution_w - gvimrect_w]
 resmart_y = [(resolution_h - gvimrect_h) / 2,resolution_h - gvimrect_h]
+big_x = 1024
+big_y = 768
+large_x = 1280
+large_y = 960
 
 def vimargument(x,y,z):
     vim.command("let g:gvimwinpos_x="+str(x))
     vim.command("let g:gvimwinpos_y="+str(y))
     vim.command("let g:gvimwinpos_mode="+str(z))
 
-def selectmode():
-    if sys.argv[0] == "normal":
-        if gvimrect_x != center_x and gvimrect_y != center_y:
-            vimargument(center_x,center_y,5)
-        elif gvimrect_x == center_x and gvimrect_y == center_y:
-            vimargument(center_left_x,center_y,4)
-        elif gvimrect_x == center_left_x and gvimrect_y == center_y:
-            vimargument(center_right_x,center_y,6)
-        elif gvimrect_x == center_right_x and gvimrect_y == center_y:
-            vimargument(center_x,center_y,5)
-    elif sys.argv[0] == "smart":
-        if wincenter[0] > 0 and wincenter[0] < smart_x[0]:
-            if wincenter[1] < smart_y[0]:
-                vimargument(0,0,7)
-            elif wincenter[1] > smart_y[0] and wincenter[1] < smart_y[1]:
-                vimargument(0,resmart_y[0],4)
-            elif wincenter[1] > smart_y[1] and wincenter[1] < smart_y[2]:
-                vimargument(0,resmart_y[1],1)
-        elif wincenter[0] > smart_x[0] and wincenter[0] < smart_x[1]:
-            if wincenter[1] < smart_y[0]:
-                vimargument(resmart_x[0],0,8)
-            elif wincenter[1] > smart_y[0] and wincenter[1] < smart_y[1]:
-                vimargument(resmart_x[0],resmart_y[0],5)
-            elif wincenter[1] > smart_y[1] and wincenter[1] < smart_y[2]:
-                vimargument(resmart_x[0],resmart_y[1],2)
-        elif wincenter[0] > smart_x[1] and wincenter[0] < smart_x[2]:
-            if wincenter[1] < smart_y[0]:
-                vimargument(resmart_x[1],0,9)
-            elif wincenter[1] > smart_y[0] and wincenter[1] < smart_y[1]:
-                vimargument(resmart_x[1],resmart_y[0],6)
-            elif wincenter[1] > smart_y[1] and wincenter[1] < smart_y[2]:
-                vimargument(resmart_x[1],resmart_y[1],3)
+def vimsize(x,y):
+    a = (resolution_w - x) / 2
+    b = (resolution_h - y) / 2
+    win32gui.SetWindowPos(hwnd, 0, a, b, x, y, 0x0004)
+    vim.command("let g:gvimwinsize_x="+str(x))
+    vim.command("let g:gvimwinsize_y="+str(y))
 
-selectmode()
+def mode_normal():
+    if gvimrect_x != center_x and gvimrect_y != center_y:
+        vimargument(center_x,center_y,5)
+    elif gvimrect_x == center_x and gvimrect_y == center_y:
+        vimargument(center_left_x,center_y,4)
+    elif gvimrect_x == center_left_x and gvimrect_y == center_y:
+        vimargument(center_right_x,center_y,6)
+    elif gvimrect_x == center_right_x and gvimrect_y == center_y:
+        vimargument(center_x,center_y,5)
+
+def mode_smart():
+   if wincenter[0] >= 0 and wincenter[0] <= smart_x[0]:
+       if wincenter[1] <= smart_y[0]:
+           vimargument(0,0,7)
+       elif wincenter[1] >= smart_y[0] and wincenter[1] <= smart_y[1]:
+           vimargument(0,resmart_y[0],4)
+       elif wincenter[1] >= smart_y[1] and wincenter[1] <= smart_y[2]:
+           vimargument(0,resmart_y[1],1)
+   elif wincenter[0] >= smart_x[0] and wincenter[0] <= smart_x[1]:
+       if wincenter[1] <= smart_y[0]:
+           vimargument(resmart_x[0],0,8)
+       elif wincenter[1] >= smart_y[0] and wincenter[1] <= smart_y[1]:
+           vimargument(resmart_x[0],resmart_y[0],5)
+       elif wincenter[1] >= smart_y[1] and wincenter[1] <= smart_y[2]:
+           vimargument(resmart_x[0],resmart_y[1],2)
+   elif wincenter[0] >= smart_x[1] and wincenter[0] <= smart_x[2]:
+       if wincenter[1] <= smart_y[0]:
+           vimargument(resmart_x[1],0,9)
+       elif wincenter[1] >= smart_y[0] and wincenter[1] <= smart_y[1]:
+           vimargument(resmart_x[1],resmart_y[0],6)
+       elif wincenter[1] >= smart_y[1] and wincenter[1] <= smart_y[2]:
+           vimargument(resmart_x[1],resmart_y[1],3)
+
+def mode_big():
+    vimsize(big_x,big_y)
+
+def mode_large():
+    vimsize(large_x,large_y)
+
+selectmode = {'normal': mode_normal,
+              'smart': mode_smart,
+              'big': mode_big,
+              'large': mode_large,
+}
+selectmode[sys.argv[0]]()
--- gvimwinpos.vim
+++ gvimwinpos.vim
@@ -1,15 +1,26 @@
 if has('python')
 	python import sys
 	function! GvimWinPos(mode)
-		if a:mode == 1
-			python sys.argv = ["smart"]
-		elseif a:mode == 0
+		if a:mode == 0
 			python sys.argv = ["normal"]
+		elseif a:mode == 1
+			python sys.argv = ["smart"]
 		endif
 		execute 'pyfile ' . '$VIM/gvimwinpos.py'
 		execute 'winpos ' . g:gvimwinpos_x g:gvimwinpos_y
 		echo "gvimwinpos_mode =" g:gvimwinpos_mode
 	endfunction
-	nnoremap <F2> :call GvimWinPos(1)<CR>
+	function! GvimWinSize(mode)
+		if a:mode == 0
+			python sys.argv = ["big"]
+		elseif a:mode == 1
+			python sys.argv = ["large"]
+		endif
+		execute 'pyfile ' . '$VIM/gvimwinpos.py'
+		echo "gvimwinsize_mode =" g:gvimwinsize_x g:gvimwinsize_y
+	endfunction
 	nnoremap <F3> :call GvimWinPos(0)<CR>
+	nnoremap <S-F3> :call GvimWinPos(1)<CR>
+	nnoremap <F2> :call GvimWinSize(0)<CR>
+	nnoremap <S-F2> :call GvimWinSize(1)<CR>
 endif
