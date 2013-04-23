845bd5628b70859779028d01f5e3391d78eed955
belldandy <belldandy@vm-debian.(none)>
2013-04-24 01:27:24 +0900

    2013-04-24_00 update

--- vim_pos+size.py
+++ vim_pos+size.py
@@ -11,19 +11,15 @@ gvimrect_x = gvimrect[0]
 gvimrect_y = gvimrect[1]
 gvimrect_w = gvimrect[2] - gvimrect_x
 gvimrect_h = gvimrect[3] - gvimrect_y
-center_x = (resolution_w - gvimrect_w) / 2
+center = [(resolution_w - gvimrect_w) / 2, 0, resolution_w - gvimrect_w]
 center_y = (resolution_h - gvimrect_h) / 2
-center_left_x = 0
-center_right_x = (resolution_w - gvimrect_w)
 smart_x = [resolution_w / 3, resolution_w / 3 * 2, resolution_w]
 smart_y = [resolution_h / 3, resolution_h / 3 * 2, resolution_h]
 wincenter = [gvimrect_x + (gvimrect_w / 2), gvimrect_y + (gvimrect_h / 2)]
 resmart_x = [(resolution_w - gvimrect_w) / 2,resolution_w - gvimrect_w]
 resmart_y = [(resolution_h - gvimrect_h) / 2,resolution_h - gvimrect_h]
-big_x = 1024
-big_y = 768
-large_x = 1280
-large_y = 960
+big = [1024, 768]
+large = [1280, 960]
 
 def vim_pos(x,y,z):
     vim.command("let g:vim_pos_x="+str(x))
@@ -38,14 +34,14 @@ def vim_size(x,y):
     vim.command("let g:vim_size_y="+str(y))
 
 def mode_normal():
-    if gvimrect_x != center_x and gvimrect_y != center_y:
-        vim_pos(center_x,center_y,5)
-    elif gvimrect_x == center_x and gvimrect_y == center_y:
-        vim_pos(center_left_x,center_y,4)
-    elif gvimrect_x == center_left_x and gvimrect_y == center_y:
-        vim_pos(center_right_x,center_y,6)
-    elif gvimrect_x == center_right_x and gvimrect_y == center_y:
-        vim_pos(center_x,center_y,5)
+    if gvimrect_x != center[0] and gvimrect_y != center_y:
+        vim_pos(center[0],center_y,5)
+    elif gvimrect_x == center[0] and gvimrect_y == center_y:
+        vim_pos(center[1],center_y,4)
+    elif gvimrect_x == center[1] and gvimrect_y == center_y:
+        vim_pos(center[2],center_y,6)
+    elif gvimrect_x == center[2] and gvimrect_y == center_y:
+        vim_pos(center[0],center_y,5)
 
 def mode_smart():
    if wincenter[0] >= 0 and wincenter[0] <= smart_x[0]:
@@ -71,10 +67,10 @@ def mode_smart():
            vim_pos(resmart_x[1],resmart_y[1],3)
 
 def mode_big():
-    vim_size(big_x,big_y)
+    vim_size(big[0],big[1])
 
 def mode_large():
-    vim_size(large_x,large_y)
+    vim_size(large[0],large[1])
 
 selectmode = {'normal': mode_normal,
               'smart': mode_smart,
